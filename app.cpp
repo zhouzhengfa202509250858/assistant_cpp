23e6211c1c7b8696eb2b3582155aab2830927cd3
zhouzhengfa <zhouzhengfa1995@gmail.com>
2025-09-27 19:27:50 +0800

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -8,3 +8,7 @@ int main(){
   return 0;
 }
 //what you call "self-media" should be named note. so your means of livelihood still is deliver takeout,
+
+
+
+//no mattet you have any think,you always need money,so you must have a positive money-mode.
